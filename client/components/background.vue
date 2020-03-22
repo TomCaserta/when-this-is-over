f63084e3694f2f056c7a61351302be6f27b8ff1c
Thomas Caserta <tomcaserta@gmail.com>
2020-03-22 12:46:45 +0100

    chore: get typescript working correctly with nuxt

---
+++ client/components/background.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="background">
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.background {
+    background: linear-gradient(157.61deg, #CA054D 23.14%, #C706B4 85.46%);
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+}
+</style>
+
+<script lang="ts">
+export default {};
+</script>
--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
+    <Background />
     <nuxt />
   </div>
 </template>
 
-<style>
+<style lang="scss">
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
@@ -23,33 +24,20 @@ html {
   box-sizing: border-box;
   margin: 0;
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
 </style>
+
+<script lang="ts">
+import { Component } from 'vue-property-decorator';
+import Vue from 'vue';
+
+import Background from '@/components/background.vue';
+
+@Component({
+    components: {
+        Background,
+    }
+})
+export default class DefaultLayout extends Vue {
+
+}
+</script>
