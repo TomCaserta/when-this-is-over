af30fe9261e24c54db43762b5564e9f34a1066bd
Rodrigo Siqueira <rodriados@gmail.com>
2020-03-26 23:56:16 -0400

    chore: use promises to init app and server

--- server/components/user/user.controller.ts
+++ server/components/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { User, IUserModel } from '~/components/user/user.model';
+import { User } from '~/components/user/user.model';
 import { Request, Response } from 'express';
 import * as jwt from 'jsonwebtoken';
 import config from 'config';
@@ -16,23 +16,21 @@ export default class UserController {
       return res.status(400).json({ error: 'missing_credentials' });
     }
 
-    let user: IUserModel;
+    return await User.findOne({ email }).orFail()
+      .then(user => {
+        if (!user || !user.checkPassword(password)) {
+          return res.status(401).json({ error: 'invalid_credentials' });
+        }
 
-    try {
-      user = await User.findOne({ email }).exec() as IUserModel;
-    } catch (error) {
-      return res.status(401).json({ error: 'invalid_credentials' });
-    }
+        const token = jwt.sign({ id: user._id, email }, secret, {
+          expiresIn: config.get('auth.expire')
+        });
 
-    if (!user.checkPassword(password)) {
-      return res.status(401).json({ error: 'invalid_credentials' });
-    }
-
-    const token = jwt.sign({ id: user._id, email }, secret, {
-      expiresIn: config.get('auth.expire')
-    });
-
-    return res.json({ token });
+        return res.json({ token });
+      })
+      .catch(() => {
+        return res.status(401).json({ error: 'invalid_credentials' });
+      });
   }
 
 }
--- server/index.ts
+++ server/index.ts
@@ -1,32 +1,12 @@
 import dotenv from 'dotenv';
 import config from 'config';
 import consola from 'consola';
-import mongoose from "mongoose";
-import { app } from '~/app';
+import { application } from '~/app';
 
 dotenv.config();
 
-const destroyServer = (message: string) => {
-  consola.fatal({
-    message,
-    badge: true
-  });
-  process.exit(1);
-};
-
-if (!config.get('auth.secret')) {
-  destroyServer('No primary key for encryption found');
-}
-
-const connection = {
-  url: config.get('db.mongo.url') as string,
-  user: config.get('db.mongo.user') as string,
-  pass: config.get('db.mongo.pass') as string
-};
-
-mongoose.connect(connection.url, { useNewUrlParser: true })
-  
-  .then(() => {
+application()
+  .then(app => {
     const host = (config.get('server.host') as string) || 'localhost';
     const port = (config.get('server.port') as number) || 3000;
 
@@ -39,8 +19,4 @@ mongoose.connect(connection.url, { useNewUrlParser: true })
         badge: true
       });
     });
-  })
-  
-  .catch(error => {
-    destroyServer('Could not connect to MongoDB')
   });
