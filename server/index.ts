2c89753bac2a684bb0ad95c1082a1583243b00b5
Thomas Caserta <tomcaserta@gmail.com>
2020-03-28 02:25:49 +0100

    fix: load config after loading dotenv

--- server/index.ts
+++ server/index.ts
@@ -1,22 +1,30 @@
 import dotenv from 'dotenv';
-import config from 'config';
 import consola from 'consola';
-import { application } from '~/app';
+import { join } from 'path';
 
-dotenv.config();
+dotenv.config({ path: join(__dirname, '.env'), debug: true });
 
-application()
-  .then(app => {
-    const host = (config.get('server.host') as string) || 'localhost';
-    const port = (config.get('server.port') as number) || 3000;
+async function main () {
+    // Load config after dotenv has been configured.
+    const config = require('config');
+    const { application } = await import('~/app');
 
-    app.set('host', host);
-    app.set('port', port);
 
-    app.listen(port, host, () => {
-      consola.ready({
-        message: `Server listening on http://${host}:${port}`,
-        badge: true
+    application()
+      .then(async app => {
+        const host = (config.get('server.host') as string) || 'localhost';
+        const port = (config.get('server.port') as number) || 3000;
+
+        app.set('host', host);
+        app.set('port', port);
+
+        app.listen(port, host, () => {
+          consola.ready({
+            message: `Server listening on http://${host}:${port}`,
+            badge: true
+          });
+        });
       });
-    });
-  });
+}
+
+main();
--- server/router.ts
+++ server/router.ts
@@ -1,8 +1,11 @@
 import { router as index } from '~/components/index/index.router';
 import { router as user } from '~/components/user/user.router';
+import { router as autocomplete } from '~/components/autocomplete/autocomplete.router';
 import { Router } from 'express';
 
 export const router = Router();
 
 router.use('/', index);
 router.use('/users', user);
+router.use('/autocomplete', autocomplete);
+
